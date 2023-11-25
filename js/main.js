3c62fcf2f8825a0e40dc99b1bb9b6cd362c93a83
sinaKarimi77 <149428847+sinaKarimi77@users.noreply.github.com>
2023-11-25 03:49:07 +0330

    Update main.js

--- js/main.js
+++ js/main.js
@@ -3,11 +3,11 @@ const lightbtn = document.getElementById("lightbtn");
 
 darkbtn.addEventListener('click',()=>{
     // document.body.style.backgroundColor ="black";
-    document.body.classList.add('text-bg-dark');
+    document.body.classList=('text-bg-dark');
     // document.body.classList.add('text-white');
 });
 lightbtn.addEventListener('click',()=>{
     // document.body.style.backgroundColor ="black";
-    document.body.classList.add('text-bg-white');
+    document.body.classList=('text-bg-white');
     // document.body.classList.add('text-white');
 });
